25cffd1b8e1c8ea4a98b3f225b840db90ab41111
Pato <pmontecchiarini@gmail.com>
2020-05-22 14:58:53 -0300

    Styles timeline not finished

---
+++ js/timeline.js
@@ -0,0 +1,51 @@
+$(function(){
+
+    window.sr = ScrollReveal();
+  
+    if ($(window).width() < 768) {
+  
+      if ($('.container').hasClass('js--fadeInLeft')) {
+          $('.container').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+      }
+  
+      sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+    } else {
+      
+      sr.reveal('.js--fadeInLeft', {
+          origin: 'left',
+          distance: '300px',
+            easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+        sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+    }
+    
+    sr.reveal('.js--fadeInLeft', {
+          origin: 'left',
+          distance: '300px',
+            easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+        sr.reveal('.js--fadeInRight', {
+          origin: 'right',
+          distance: '300px',
+          easing: 'ease-in-out',
+          duration: 800,
+        });
+  
+  
+  });
