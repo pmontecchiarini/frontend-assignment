888c74134e46a216d874c5c822f0e8960fbfdd86
Pato <pmontecchiarini@gmail.com>
2020-05-23 01:21:08 -0300

    Styled timeline

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ Styled with mobile first, responsive code. SASS, HTML5.
 
 *No error shown by markup validator (W3C)
 
+Brower support: Chrome, Firefox, Opera.
+
 Total time to complete assignment:
--- js/timeline.js
+++ js/timeline.js
@@ -1,51 +1,33 @@
-$(function(){
+(function() {
 
-    window.sr = ScrollReveal();
-  
-    if ($(window).width() < 768) {
-  
-      if ($('.container').hasClass('js--fadeInLeft')) {
-          $('.container').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
+  'use strict';
+
+  // define variables
+  var items = document.querySelectorAll(".timeline li");
+
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
       }
-  
-      sr.reveal('.js--fadeInRight', {
-          origin: 'right',
-          distance: '300px',
-          easing: 'ease-in-out',
-          duration: 800,
-        });
-  
-    } else {
-      
-      sr.reveal('.js--fadeInLeft', {
-          origin: 'left',
-          distance: '300px',
-            easing: 'ease-in-out',
-          duration: 800,
-        });
-  
-        sr.reveal('.js--fadeInRight', {
-          origin: 'right',
-          distance: '300px',
-          easing: 'ease-in-out',
-          duration: 800,
-        });
-  
     }
-    
-    sr.reveal('.js--fadeInLeft', {
-          origin: 'left',
-          distance: '300px',
-            easing: 'ease-in-out',
-          duration: 800,
-        });
-  
-        sr.reveal('.js--fadeInRight', {
-          origin: 'right',
-          distance: '300px',
-          easing: 'ease-in-out',
-          duration: 800,
-        });
-  
-  
-  });
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+
+})();
